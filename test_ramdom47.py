fdbc85d22580a8959887742761a6329f349fe94f
terry-w <color1252002@gmail.com>
2023-09-26 15:19:14 +0800

    ver1_48回最基本

---
+++ test_ramdom47.py
@@ -0,0 +1,22 @@
+#產生隨機整數1~100 (不要印出來)
+#讓使用者重複輸入去猜
+#猜對的話 印出答對
+#猜錯的話要告訴他 比答案大或小
+
+
+import random
+
+r = random.randint(1, 100)
+
+while True :
+	#print(r)
+	user = input("請猜一個數字")
+	if int(user) > int(r) :
+		print(" user > answer　") 
+	elif int(user) < int(r) :
+		 print(" user < answer　") 
+	else :
+		if int(user) == int(r) :
+			print("bingo")
+	#break #break放這是每猜完一次結束　並重執行　不同數字
+			break #猜錯就持續猜　並到最後對時才結束
